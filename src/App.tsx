95c1d470f2a413ee68f53fab7e85f5b6d6e44f35
jskomal <90876852+jskomal@users.noreply.github.com>
2022-11-28 21:17:24 -0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import tomato from './assets/tomato-svgrepo-com.svg'
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>pomPlayer</h1>
+			<img className='tomato' src={tomato} alt='Pomadoro tomato' />
+		</div>
+	)
+}
+
+export default App
