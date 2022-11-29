a020519d4baffd01697dfa002cd831e88beddd71
jskomal <90876852+jskomal@users.noreply.github.com>
2022-11-29 01:02:12 -0700

    WIP input control

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,16 +1,23 @@
 import tomato from '../assets/tomato-svgrepo-com.svg'
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 type Props = {}
 
 const Timer = (props: Props) => {
 	dayjs.extend(duration)
-	const [timeLeft, setTimeLeft] = useState(
-		dayjs.duration({ minutes: 0, seconds: 10 })
+	const [inputTime, setInputTime] = useState(25)
+	const [timeLeft, setTimeLeft] = useState<duration.Duration>(
+		dayjs.duration({ minutes: inputTime, seconds: 0 })
+	)
+	const [totalTime] = useState(
+		dayjs.duration({ minutes: inputTime, seconds: 0 }).as('milliseconds')
 	)
 	const [isTimerStopped, setIsTimerStopped] = useState(true)
+	const [timeLeftPercent, setTimeLeftPercent] = useState(1)
+	const [userMessage, setUserMessage] = useState('')
+	const inputRef = useRef(null)
 
 	useEffect(() => {
 		let interval: number | undefined
@@ -18,11 +25,11 @@ const Timer = (props: Props) => {
 		if (!isTimerStopped) {
 			interval = setInterval(() => {
 				setTimeLeft((prev) => {
-					if (prev.get('seconds') !== 0) {
-						return prev.subtract(1, 's')
-					} else {
+					if (prev.get('seconds') === 0 && prev.get('minutes') === 0) {
 						clearInterval(interval)
 						return prev
+					} else {
+						return prev.subtract(1, 's')
 					}
 				})
 			}, 1000)
@@ -35,12 +42,27 @@ const Timer = (props: Props) => {
 		}
 	}, [isTimerStopped])
 
+	useEffect(() => {
+		setTimeLeftPercent(() => timeLeft.as('milliseconds') / totalTime)
+	}, [timeLeft])
+
+	const handleInputTime = () => {}
+
 	return (
 		<div>
 			<img className='tomato' src={tomato} alt='Pomadoro Tomato Timer' />
+			<p>{userMessage}</p>
 			<p className='timer'>{timeLeft.format('m:ss')}</p>
 			<button onClick={() => setIsTimerStopped(false)}>start timer</button>
 			<button onClick={() => setIsTimerStopped(true)}>stop timer</button>
+			{isTimerStopped && (
+				<input
+					type='number'
+					value={inputTime}
+					onChange={(e) => setInputTime(parseInt(e.target.value))}
+				/>
+			)}
+			<button onClick={handleInputTime}>set time</button>
 		</div>
 	)
 }
